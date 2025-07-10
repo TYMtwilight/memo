fa7700011711fa1e8e5b53a5ad07d234b1f594d1
Hiroki <ore01100001binary@gmail.com>
2025-07-10 22:40:10 +0900

    ✍️React+Spring  メモアプリ作成-3

--- memo/src/main/java/com/example/memo/controller/MemoController.java
+++ memo/src/main/java/com/example/memo/controller/MemoController.java
@@ -2,6 +2,7 @@ package com.example.memo.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/memos")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MemoController {
     private final MemoService memoService;
 
@@ -53,4 +55,3 @@ public class MemoController {
         memoService.delete(id);
     }
 }
-    
